e3e08a061d443bae5c8c421b412f6b34ffda6953
komisan19 <komiyama6219@gmail.com>
2018-02-21 22:48:27 +0900

    fix

--- Gemfile
+++ Gemfile
@@ -3,6 +3,6 @@ source "https://rubygems.org"
 
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
-ruby 2.4.2
 
 gem "twitter"
+gem "dotenv"
--- main.rb
+++ main.rb
@@ -1,18 +1,20 @@
 require 'net/http'
 require 'uri'
+require 'dotenv'
 require 'json'
 require 'twitter'
 
 # Twitter認証
+Dotenv.load
 @client = Twitter::REST::Client.new do |config|
-  config.consumer_key = "Your key"
-	config.consumer_secret = "Your key"
-	config.access_token = "Your key"
-	config.access_token_secret = "Your key"
+  config.consumer_key = ENV["TWITTER_CONSUMER_KEY"]
+	config.consumer_secret = ENV["TWITTER_CONSUMER_SECRET"]
+	config.access_token = ENV["TWITTER_ACCESS_TOKEN"]
+	config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
 end
 
 # livedoor天気より
-  uri = URI.parse('http://weather.livedoor.com/forecast/webservice/json/v1?city=120010')　#=> 千葉
+  uri = URI.parse('http://weather.livedoor.com/forecast/webservice/json/v1?city=120010') #=> 千葉
   json = Net::HTTP.get(uri)
   result = JSON.parse(json)
 
